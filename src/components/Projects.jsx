67f1720c3254beb27d1b354d1619d957a9bf7a60
Darshan <darsuprajapati262@gmail.com>
2025-07-13 18:10:32 +0530

    css changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,9 +14,11 @@ const Projects = () => {
                     <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:1}} className="w-full max-w-xl lg:w-3/4">
                         <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
                         <p className="mb-4 text-stone-400">{project.description}</p>
+                        <div className="flex flex-wrap">
                         {project.technologies.map((tech,index)=>(
                             <span key={index} className="mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300">{tech}</span>
                         ))}
+                        </div>
                     </motion.div>
                 </div>
             ))}
