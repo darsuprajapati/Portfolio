b1dd79789af8ecaf0478066ffc7dc374b864d45b
Darshan <darsuprajapati262@gmail.com>
2025-07-15 22:48:48 +0530

    css changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
                 <motion.div className="flex flex-col items-center lg:items-start mt-10" initial="hidden" animate="visible" variants={containerVariants}>
                     <motion.h2 variants={childVariants} className="pb-2 text-4xl tracking-tighter lg:text-7xl">Darshan Prajapati</motion.h2>
                     <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</motion.span>
-                    <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+                    <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter  text-center sm:text-left ">
                         {HERO_CONTENT}
                     </motion.p>
                     <motion.a variants={childVariants} href={resume} target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume</motion.a>
